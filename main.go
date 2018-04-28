e568bb8aa42b1e7af0fd876d4f178df44db2cafd
A <alexuserid@gmail.com>
2018-04-28 23:31:54 +0300

    Create readme, main, tr

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+//	"archive/zip"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+//	"strings"
+)
+
+type bt struct {
+	block string
+	tr string
+}
+
+var m = make(map[string]bt)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	trans := r.URL.Query().Get("t")
+	j, err := json.Marshal(m[trans])
+	if err != nil {
+		fmt.Println("json.Marshal: ", err)
+	}
+	fmt.Fprintf(w, "%v\n", j)
+}
+
+
+func main() {
+	reader, err := ioutil.ReadFile("tr")
+	if err != nil {
+		fmt.Println("ioutil.Readfile: ", err)
+	}
+
+	fieldsB := bytes.Fields(reader)
+	for i:=0; i<len(fieldsB); i+=3 {
+		m[string(fieldsB[i])] = bt{string(fieldsB[i+1]), string(fieldsB[i+2])}
+	}
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
