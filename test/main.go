48e0572a444c84afa78251dad88c305ca3b4aa4b
A <alexuserid@gmail.com>
2018-05-01 21:36:35 +0300

    Update test

--- test/main.go
+++ test/main.go
@@ -3,47 +3,101 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
-	"strings"
 	"time"
-	"unicode"
+	"os"
+	"math/rand"
+	"compress/gzip"
+	"encoding/json"
 )
 
 var (
-	addr = flag.String("a", "http://localhost:8080", "Server address for test")
-	dur  = flag.Duration("d", 5*time.Second, "Test duration")
+	addr = flag.String("address", "http://localhost:8080", "Server address for test")
+	dur  = flag.Duration("duration", 5*time.Second, "Test duration")
+	testFile = flag.String("test", "../tr.txt.gz", "Test file")
+	n = flag.Int("number", 10, "Test items number")
 )
+
+type st struct {
+	k string
+	v1, v2 int
+}
+type bt struct {
+	Block,Tr int
+}
+
+func shuf(n *int, testFile *string) []st {
+	openf, err := os.Open(*testFile)
+	if err != nil {
+		log.Fatalf("os.Open: %v", err)
+	}
+	defer openf.Close()
+
+	gzReader, err := gzip.NewReader(openf)
+	if err != nil {
+		log.Fatalf("gzip.NewReader: %v", err)
+	}
+
+	var (
+		key string
+		val1, val2, i, j int
+		randMass []st
+	)
+	for {
+		_, err := fmt.Fscanln(gzReader, &key, &val1, &val2)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatalf("fmt.Fscanln: %v", err)
+		}
+
+		if i < *n {
+			randMass = append(randMass, st{key, val1, val2})
+		} else {
+			j = rand.Intn(i)
+			if j < *n {
+				randMass[j] = st{key, val1, val2}
+			}
+		}
+		i++
+	}
+	for _, value := range wrongMass {
+		j = rand.Intn(i)
+		if j < *n {
+			randMass = append(randMass, value)
+		}
+		i++
+	}
+	return randMass
+}
 
 func main() {
 	flag.Parse()
+
+	mix := shuf(n, testFile)
+
 	timer := time.NewTimer(*dur)
-	var i float64
+	var i int
 
 	for {
 		select {
 		case <-timer.C:
-			fmt.Printf("Timeout. Test was repeated %g times. Duration %v. %vrps.\n", i, *dur, i/dur.Seconds())
+			fmt.Printf("Timeout. Test was repeated %d times. Duration %v. %vrps.\n", i, *dur, float64(i)/dur.Seconds())
 			return
 		default:
-			for j, v := range mass {
+			for j, v := range mix {
 				resp, err := http.Get(*addr + "?t=" + v.k)
 				if err != nil {
 					log.Fatalf("http.Get: %v", err)
 				}
 
-				by, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-					log.Fatalf("ioutil.ReadAll: %v", err)
-				}
+				var dec bt
+				json.NewDecoder(resp.Body).Decode(&dec)
 
-				trims := strings.TrimFunc(string(by), func(r rune) bool {
-					return !unicode.IsNumber(r)
-				})
-				sp := strings.Split(trims, `,"Tr":`)
-				if (sp[0] != v.v1 || sp[1] != v.v2) && v.v1 != "wa" {
-					fmt.Printf("Wrong answer.\nTest %d: %q.\nServer answer is: %q.\nRight answer is: %q.\n", j, v.v1, string(by), v.v2)
+				if (dec.Block != v.v1 || dec.Tr != v.v2) && v.v1 != 0 {
+					fmt.Printf("Wrong answer.\nTest %d: %q.\nServer answer is: %q.\nRight answer is: %q.\n", j, v.v1, dec, v.v2)
 					return
 				}
 				i++
